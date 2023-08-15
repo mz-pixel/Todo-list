98bdff317ca6d966b302aa0665c4a7e74b8d9310
Muhammad Zamin <63674082+mz-pixel@users.noreply.github.com>
2023-08-15 15:11:35 -0400

    connected the app to mongoDB database

--- index.js
+++ index.js
@@ -1,64 +1,168 @@
+//jshint esversion:6
+
 import express from "express";
 import bodyParser from "body-parser";
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { ObjectId } from "mongodb";
+import _ from "lodash";
 
 const app = express();
 const PORT = 3000;
-const url = "mongodb://localhost:27017";
-const dbName = "todolistDB";
+const url =
+  "mongodb+srv://zaminjamalzj:hekpnxjISdk3q9Oe@cluster0.bnrho8k.mongodb.net/todolistDB";
 
-var TodaylistOfTask = [];
-var WorklistOfTask = [];
-let lastDate = null;
+let TodaylistOfTask;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  const now = new Date();
-  if (lastDate && now.getDate() !== lastDate.getDate()) {
-    TodaylistOfTask = [];
-    WorklistOfTask = [];
-  }
-  lastDate = new Date();
-  const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  const day = now.toLocaleDateString("en-US", options);
+async function connectToMongoDB() {
+  const client = new MongoClient(url, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
 
-  if (TodaylistOfTask.length == 0) {
-    res.render("index.ejs", { type: day });
-  } else {
-    res.render("index.ejs", {
-      type: day,
-      list: TodaylistOfTask,
-    });
-  }
-});
-app.get("/work", (req, res) => {
-  if (WorklistOfTask.length == 0) {
-    res.render("work.ejs", { type: "Work" });
-  } else {
-    res.render("work.ejs", {
-      type: "Work",
-      list: WorklistOfTask,
-    });
-  }
-});
-app.post("/", (req, res) => {
-  TodaylistOfTask.push(req.body["input"]);
-  res.redirect("/");
-});
-app.post("/work", (req, res) => {
-  WorklistOfTask.push(req.body["input"]);
-  res.redirect("/work");
-});
-app.get("/about", (req, res) => {
-  res.render("about.ejs");
-});
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+    const db = await client.db("todolistDB");
+
+    app.get("/", async (req, res) => {
+      TodaylistOfTask = await db
+        .collection("Today")
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err;
+          console.log(result);
+          db.close();
+        });
+      if (TodaylistOfTask.length == 0) {
+        await db
+          .collection("Today")
+          .insertMany([
+            { task: "Welcome to your todo list!" },
+            { task: "Press enter key or submit button to add a new item." },
+            { task: "<-- Hit this to delete an item." },
+          ]);
+        TodaylistOfTask = await db
+          .collection("Today")
+          .find({})
+          .toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+          });
+        res.render("index.ejs", { type: "Today", list: TodaylistOfTask });
+      } else {
+        res.render("index.ejs", {
+          type: "Today",
+          list: TodaylistOfTask,
+        });
+      }
+    });
+
+    app.get("/about", (req, res) => {
+      res.render("about.ejs");
+    });
+
+    app.post("/about", (req, res) => {
+      res.redirect("/about");
+    });
+
+    app.get("/:customListName", async (req, res) => {
+      const customListName = req.params.customListName;
+      const customListNameCapitalized = _.capitalize(customListName);
+      var customList = await db
+        .collection(customListNameCapitalized)
+        .find({})
+        .toArray(function (err, result) {
+          if (err) throw err;
+          console.log(result);
+          db.close();
+        });
+      if (customList.length == 0) {
+        await db
+          .collection(customListNameCapitalized)
+          .insertMany([
+            { task: "Welcome to your todo list!" },
+            { task: "Press enter key or submit button to add a new item." },
+            { task: "<-- Hit this to delete an item." },
+          ]);
+        customList = await db
+          .collection(customListNameCapitalized)
+          .find({})
+          .toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+          });
+        res.render("index.ejs", {
+          type: customListNameCapitalized,
+          list: customList,
+        });
+      } else {
+        res.render("index.ejs", {
+          type: customListNameCapitalized,
+          list: customList,
+        });
+      }
+    });
+
+    app.post("/", async (req, res) => {
+      const newTask = req.body["input"];
+      const listN = await req.body.something;
+      console.log(listN);
+      const listName = _.capitalize(listN);
+      if (listName === "Today") {
+        const result = await db
+          .collection("Today")
+          .insertOne({ task: newTask });
+        res.redirect("/");
+      } else {
+        const result = await db
+          .collection(listName)
+          .insertOne({ task: newTask });
+        res.redirect("/" + listName);
+      }
+    });
+
+    app.post("/delete", async (req, res) => {
+      const checkedItemId = req.body.checkbox;
+      const listName = req.body.hidden;
+      const capListName = _.capitalize(listName);
+      const checkedItemObjectID = new ObjectId(checkedItemId);
+      if (listName !== "Today") {
+        const result = await db
+          .collection(capListName)
+          .deleteOne({ _id: checkedItemObjectID });
+        setTimeout(() => {
+          res.redirect("/" + listName);
+        }, 500);
+      } else {
+        const result = await db
+          .collection("Today")
+          .deleteOne({ _id: checkedItemObjectID });
+        setTimeout(() => {
+          res.redirect("/");
+        }, 500);
+      }
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error:", err);
+  } finally {
+    // await client.close(function (err, result) {
+    //   if (err) throw err;
+    //   console.log(result);
+    // });
+  }
+}
+
+connectToMongoDB().catch(console.error);
